360ffaf963dfdccdebcd24979cebfbf2f4c32377
codewinterll <3104909688@qq.com>
2021-04-13 22:48:36 +0800

    更新代码

--- test_4_13/test_4_13/test.c
+++ test_4_13/test_4_13/test.c
@@ -1,1 +1,13 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+#include <string.h>
+#include <stdio.h>
+
+void main(void)
+{
+	char string[80];
+	strcpy(string, "Hello world from ");
+	strcat(string, "strcpy ");
+	strcat(string, "and ");
+	strcat(string, "strcat!");
+	printf("String = %s\n", string);
+}
