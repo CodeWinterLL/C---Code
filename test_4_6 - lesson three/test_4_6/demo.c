a88e37821737668526953a7aaf01a1c79ffef987
codewinterll <3104909688@qq.com>
2021-04-24 23:06:30 +0800

    更新代码

--- test_4_6 - lesson three/test_4_6/demo.c
+++ test_4_6 - lesson three/test_4_6/demo.c
@@ -88,20 +88,112 @@ b - 10000000000000000000000000000010（原码）(-2)
 //	return 0;
 //}
 
-//关键字：static
+////关键字：static 修饰局部变量
+//#include<stdio.h>
+//void test()
+//{
+//	static int a = 1;
+//	a++;
+//	printf("%d ", a);
+//}
+//int main()
+//{
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		test();
+//	}
+//	return 0;
+//}
+
+
+//关键字：static 修饰全局变量
+//#include<stdio.h>
+//声明全局变量
+//extern int g_val;
+//int main()
+//{
+//	printf("%d\n", g_val);
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+////声明demo2.c中的函数
+//extern int add(int, int);
+//int main()
+//{
+//	int a = 1;
+//	int b = 2;
+//	int c = add(a, b);
+//	printf("a+b=%d\n", c);
+//	return 0;
+//}
+
+
+//#define 定义常量和宏
+//普通宏
+//#define PI (3.1415926)
+////带参数的宏
+//#define MAX(a,b) ( (a) > (b) ? (a) : (b) )
+////关键是十分容易产生错误，包括机器和人理解上的差异等等。
+//#include<stdio.h>
+//int main()
+//{
+//    int a = ((2) > (3) ? (2) : (3));
+//    printf("%d\n", a);
+//    return 0;
+//    int a = 2 * MAX(2, 3);
+//}
+
+//指针
+//#include<stdio.h>
+//int main()
+//{
+//	//num在内存要被分配 4字节 的内存空间
+//	int num = 10;
+//	//%p - 以地址的形式打印
+//	printf("%p\n", &num);
+//	//变量的存储
+//	int* p = &num;
+//	//* - 解引用操作符，访问指针p指向的内存空间
+//	*p = 30;
+//	printf("%d\n", *p);
+//	//指针变量的大小
+//	printf("%d字节\n", sizeof(p));
+//	printf("%d字节\n", sizeof(char *));
+//	printf("%d字节\n", sizeof(int *));
+//	printf("%d字节\n", sizeof(double *));
+//
+//	return 0;
+//}
+
+
+//结构体
 #include<stdio.h>
-void test()
+
+struct Student
 {
-	static int a = 1;
-	a++;
-	printf("%d ", a);
-}
+	char name[20];  //姓名
+	int age;  //年龄
+	double grade;  //成绩
+};
+
 int main()
 {
-	int i = 0;
-	for (i = 0; i < 10; i++)
-	{
-		test();
-	}
+	//定义一个结构体变量并初始化
+	struct Student s1 = { "玛卡巴卡",20,85 };
+	
+	/*访问结构体变量中的成员*/
+	
+	//. 结构体成员访问操作符
+	printf("name: %s age: %d grade: %lf\n", s1.name, s1.age, s1.grade);
+	
+	//用结构体指针访问
+	struct Student* ps = &s1;
+	printf("name: %s age: %d grade: %lf\n", ps->name, ps->age, ps->grade);  //->操作符
+	//还可以这样，不过太麻烦，建议用 ->操作符 访问更直观
+	printf("name: %s age: %d grade: %lf\n", (*ps).name, (*ps).age, (*ps).grade);
+	
 	return 0;
 }
---
+++ test_4_6 - lesson three/test_4_6/demo2.c
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//全局变量
+//static int g_val = 2021;
+
+//函数
+static int add(int x, int y)
+{
+	return x + y;
+}
