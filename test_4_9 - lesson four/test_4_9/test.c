62ebb35c7c964ff858f0f6f9c9069888ce34c485
codewinterll <3104909688@qq.com>
2021-05-06 22:52:47 +0800

    更新代码

--- test_4_9 - lesson four/test_4_9/test.c
+++ test_4_9 - lesson four/test_4_9/test.c
@@ -1,16 +1,26 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-	int a = 0;
-	int b = 2;
-
-	if (a == 1)
-		if (b == 2)
-			printf("hehe\n");
-		else
-			printf("hehe\n");
-
-	return 0;
+    int day = 0;
+    scanf("%d", &day);
+    switch (day)
+    {
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+            printf("工作日\n");
+            break;
+        case 6:
+        case 7:
+            printf("休息日\n");
+            break;
+        default:
+            printf("输入错误\n");
+            break;
+    }
+    return 0;
 }
