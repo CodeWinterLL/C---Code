b8c9796f8936911ae25ffdaa073ed13a62ff2741
codewinterll <3104909688@qq.com>
2021-05-07 22:59:46 +0800

    更新代码

--- test_4_9 - lesson four/test_4_9/test.c
+++ test_4_9 - lesson four/test_4_9/test.c
@@ -1,26 +1,53 @@
-#define _CRT_SECURE_NO_WARNINGS 1
+//分支与循环
 
-#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 1;
+//	int j = 1;
+//	while (1)  //外层循环
+//	{
+//		j = 1;
+//		while (1)  //内层循环
+//		{
+//			printf("%d ", i * j);
+//			j++;
+//			if (j > 3)
+//				break;  //跳出内层循环
+//		}
+//		printf("\n");
+//		i++;
+//		if (i > 3)
+//			break;  //跳出外层循环
+//	}
+//    return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	while (i <= 10)
+//	{
+//		i++;
+//		if (i == 5)
+//			continue;
+//		printf("%d ", i);
+//		//i++;
+//	}
+//	return 0;
+//}
+
+#include<stdio.h>
 int main()
 {
-    int day = 0;
-    scanf("%d", &day);
-    switch (day)
-    {
-        case 1:
-        case 2:
-        case 3:
-        case 4:
-        case 5:
-            printf("工作日\n");
-            break;
-        case 6:
-        case 7:
-            printf("休息日\n");
-            break;
-        default:
-            printf("输入错误\n");
-            break;
-    }
-    return 0;
+	int ch = 0;
+	while ((ch = getchar()) != EOF)
+	{
+		putchar(ch);
+	}
+	return 0;
 }
+
+
