bbcc5b0e260824371b3f5764045653da875eb165
codewinterll <3104909688@qq.com>
2021-05-08 23:32:46 +0800

    更新代码

--- test_5_7/test_5_7/test.c
+++ test_5_7/test_5_7/test.c
@@ -3,7 +3,30 @@
 #include<stdio.h>
 int main()
 {
+	char password[20] = { 0 };
+	
+	printf("Input password:");
+	scanf("%s", password);
+	
+	//清理缓冲区
+	//getchar();  //处理 '\n'
+	int temp = 0;
+	while ((temp = getchar()) != '\n')
+	{
+		;
+	}
 
-
+	printf("Comfirm password(Y/N):");
+	int ch = 0;
+	ch = getchar();
+	
+	if (ch == 'Y')
+	{
+		printf("success!\n");
+	}
+	else
+	{
+		printf("defeat!\n");
+	}
 	return 0;
 }
