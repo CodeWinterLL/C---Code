351d4d5e82c71b30365797c171c27af0c2787eb2
codewinterll <3104909688@qq.com>
2021-05-14 23:38:14 +0800

    更新代码

--- test_5_9/test_5_9/test.c
+++ test_5_9/test_5_9/test.c
@@ -1,13 +1,50 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+
+
+//int Add(int x, int y)
+//{
+//	return x + y;
+//}
+//
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 3;
+//	int b = 5;
+//	//() - 函数调用操作符
+//	int ret = Add(a, b);
+//	return 0;
+//}
+
+
 #include<stdio.h>
+
+//创建一个书的结构体类型
+//struct Book
+//{
+//	char name[20];
+//	char id[20];
+//	int price;
+//};
+//
+//int main()
+//{
+//	struct Book b = { "C程序设计","CX2021",48 };
+//	
+//	//(1) 结构体变量.结构体成员
+//	printf("name:%s  id:%s  price:%d", b.name, b.id, b.price);
+//	
+//	//(2) 结构体变量->结构体成员
+//	struct Book* p = &b;
+//	printf("name:%s  id:%s  price:%d", p->name, p->id, p->price);
+//
+//	return 0;
+//}
+
 int main()
 {
-	int arr[10] = { 0 };
-	char* p = arr;
-	
-	int i = 0;
-	for (i = 0; i < 10; i++)
-	{
-		*(p + i) = 1;
-	}
+	char arr1[5] = { 'a','b','c' };
+	char a = 'a';
+
+	return 0;
 }
